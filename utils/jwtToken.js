0b7779ca60f49ea9584e5a48c1181b89270381ca
Đỗ Hùng <dohung2ka3@gmail.com>
2024-03-05 08:27:25 +0700

    fix cookies

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -11,7 +11,8 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
+        secure: false,
+        sameSite: "none"
     };
 
     res.status(statusCode).cookie("token", token, options).json({
