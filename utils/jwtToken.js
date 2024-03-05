ca27ff3c53f68e5c90a3fb1d689541808135a78e
Đỗ Hùng <dohung2ka3@gmail.com>
2024-03-05 08:31:32 +0700

    add httpOnly again

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -11,8 +11,7 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        secure: false,
-        sameSite: "none"
+        httpOnly: true,
     };
 
     res.status(statusCode).cookie("token", token, options).json({
